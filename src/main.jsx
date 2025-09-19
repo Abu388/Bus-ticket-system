99c752665fc8bb0126f3ca0a47b37def9b25ad6e
Abenezer <150917473+Abu388@users.noreply.github.com>
2025-09-19 10:42:46 +0300

    back

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { BookingProvider } from "./BookingContext";
 import './index.css'
-import { HashRouter } from "react-router-dom";
-
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BookingProvider>
     <BrowserRouter>
-    <HashRouter>
       <App />
-      </HashRouter>
     </BrowserRouter>
     </BookingProvider>
   
